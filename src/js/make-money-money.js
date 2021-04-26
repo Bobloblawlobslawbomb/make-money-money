d54fed7b095d06e368407d688776b10e16653da3
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-25 21:40:02 -0700

    WIP:refactor code to be closer to working

--- src/js/make-money-money.js
+++ src/js/make-money-money.js
@@ -1,43 +1,14 @@
-export default class MoneyService {
-  static getConversion() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function (response) {
+export default class MoneyService {  
+  static getConversion(currencyCode, dollarAmount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyCode}/${dollarAmount}`)
+      .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
         return response.json();
       })
-      .catch(function (error) {
-        return error;//rm .message after error
+      .catch(function(error) {
+        return error;
       });
   }
 }
-
-// export default class PokemonService {
-//   static getPokemon(pocketMonsterName) {
-//     return fetch(`https://pokeapi.co/api/v2/pokemon/${pocketMonsterName}`)
-//       .then(function (response) {
-//         if (!response.ok) {
-//           throw Error(response.statusText);
-//         }
-//         return response.json();
-//       })
-//       .catch(function (error) {
-//         return error;
-//       });
-//   }
-// }
-
-// export default class WeatherService {  
-//   static async getWeather(city) {
-//     try {
-//       const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-//       if (!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       return response.json();
-//     } catch(error) {
-//       return error.message;
-//     }
-//   }
-// }

f56b1eb43f82af7d2ce56cbf31982a548d1d4111
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-25 21:40:38 -0700

    WIP: refactor UI to be closer to working

--- src/main.js
+++ src/main.js
@@ -4,20 +4,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import MoneyService from './js/make-money-money.js';
 
-function showConversion(response) {
-  if (response.result === "success") {
-    $('#show-conversion').text(`The amount specified in ${response.base_code} is: `); //jquery the answer here after 'is:'
+function getElements(response) {
+  if (response.success) {
+    $('#show-conversion').text(/*`the string that displays all the stuff we got from the API`*/);  //(`The humidity in ${response.name} is ${response.main.humidity}%`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('#showErrors').text(`There was an error: ${response.message}`); //this line may need tinkering
   }
 }
 
-async function parseMoney() {
-  const response = await MoneyService.getConversion();
-  if (response instanceof Error) {
-    console.log("Hey! This is an error!");
-  } else {
-    showConversion(response);
-    $("#could-not-find").hide();
-  }
-}
+$(document).ready(function() {
+  $('#pick-a-currency').click(function() {
+    let currencyCode = $('#radio-div').val();
+    let dollarAmount = $('#inputted-dollar-amount').val();// not done here, think i need to either jumble everything to only require one click/button or rework the the function in some way
+    MoneyService.getConversion(currencyCode, dollarAmount)
+      .then(function(response) {
+        getElements(response);
+      });
+  });
+});
+
+// function showConversion(response) {
+//   if (response.result === "success") {
+//     $('#show-conversion').text(`The amount specified in ${response.base_code} is: `); //jquery the answer here after 'is:'
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response.message}`);
+//   }
+// }
+
+// async function parseMoney() {
+//   const response = await MoneyService.getConversion();
+//   if (response instanceof Error) {
+//     console.log("Hey! This is an error!");
+//   } else {
+//     showConversion(response);
+//     $("#could-not-find").hide();
+//   }
+// }

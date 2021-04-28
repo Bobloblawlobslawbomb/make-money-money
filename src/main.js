c604c8fb45f7739191661a13883209f860ed81d6
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-28 07:10:56 -0700

    1 form 1 button and new main.js

--- src/main.js
+++ src/main.js
@@ -6,37 +6,23 @@ import MoneyService from './js/make-money-money.js';
 
 function getElements(response) {
   if (response.success) {
-    $('#show-conversion').text(/*`the string that displays all the stuff we got from the API`*/);  //(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('#show-conversion').text(`That many ${response.base_code} converted to ${response.target_code} is ${response.conversion_result}`);
   } else {
     $('#showErrors').text(`There was an error: ${response.message}`); //this line may need tinkering
   }
 }
 
 $(document).ready(function() {
-  $('#pick-a-currency').click(function() {
-    let currencyCode = $('#radio-div').val();
-    let dollarAmount = $('#inputted-dollar-amount').val();// not done here, think i need to either jumble everything to only require one click/button or rework the the function in some way
+  let currencyCode;
+  let dollarAmount;
+  $('form-one').submit(function(){
+    currencyCode = $("#radio-div input[type='radio']:checked").val();
+    console.log("this is the currency code value: " + currencyCode);
+    dollarAmount = $('#inputted-dollar-amount').val();
+    console.log("and the dollar amount to convert: " + dollarAmount);
     MoneyService.getConversion(currencyCode, dollarAmount)
       .then(function(response) {
         getElements(response);
       });
   });
 });
-
-// function showConversion(response) {
-//   if (response.result === "success") {
-//     $('#show-conversion').text(`The amount specified in ${response.base_code} is: `); //jquery the answer here after 'is:'
-//   } else {
-//     $('.showErrors').text(`There was an error: ${response.message}`);
-//   }
-// }
-
-// async function parseMoney() {
-//   const response = await MoneyService.getConversion();
-//   if (response instanceof Error) {
-//     console.log("Hey! This is an error!");
-//   } else {
-//     showConversion(response);
-//     $("#could-not-find").hide();
-//   }
-// }
